349b46bd25f746745259aae2f6105a487c0de7e3
Onimock <murillonft.com@gmail.com>
2024-10-19 21:25:09 -0300

    Update: put more content for message

--- models/content.py
+++ models/content.py
@@ -1,11 +1,27 @@
+from dataclasses import field
+
+
 class Content:
     def __init__(self, message, user_name=None):
         self.user_name = user_name
         self.message = message
 
-    def format_payload(self):
+    def format_payload(self, original_user=None, original_message=None):
         payload = {
             "content": self.message,
-            "username": self.user_name
+            "username": self.user_name,
         }
-        return {k: v for k, v in payload.items() if v is not None}
+        if original_user:
+            payload["embeds"] = [{
+                "description": "*repply*:",
+                "fields": [
+                    {
+                        "name": original_user,
+                        "value": original_message or "",
+                        "inline": False
+                    }
+                ],
+                "footer": {"text": "Powered by Onimock"},
+            }]
+
+        return payload
