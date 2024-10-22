5900cfacac8e04e3e127bc5255c4d0a0a9e5e61f
Onimock <murillonft.com@gmail.com>
2024-10-22 09:53:57 -0300

    Refactor: embed methods

--- models/content.py
+++ models/content.py
@@ -1,3 +1,6 @@
+from models.embed import Embed
+
+
 class Content:
     def __init__(self, username):
         self.username = username
@@ -5,7 +8,6 @@ class Content:
         self.avatar_url = None
         self.content = ""
         self.embeds = []
-        self.current_embed = None
 
     def set_content(self, content):
         self.content = content
@@ -16,58 +18,21 @@ class Content:
     def set_avatar_url(self, avatar_url):
         self.avatar_url = avatar_url
 
-    def add_embed(self, description, title=None):
-        embed = {
-            "title": title,
-            "description": description,
-            "author": None,
-            "fields": [],
-            "thumbnail": None,
-            "image": None,
-            "footer": None,
-            "color": None
-        }
+    def add_embed(self, embed: Embed):
         self.embeds.append(embed)
-        self.current_embed = embed
-        return embed
 
-    def set_author(self, name, url=None, icon_url=None):
-        if self.current_embed is not None:
-            self.current_embed["author"] = {"name": name, "url": url, "icon_url": icon_url}
-
-    def add_field(self, name, value, inline=False):
-        if self.current_embed is not None:
-            self.current_embed["fields"].append({"name": name, "value": value, "inline": inline})
-
-    def set_thumbnail(self, url):
-        if self.current_embed is not None:
-            self.current_embed["thumbnail"] = {"url": url}
-
-    def set_image(self, url):
-        if self.current_embed is not None:
-            self.current_embed["image"] = {"url": url}
-
-    def set_footer(self, text, icon_url=None):
-        if self.current_embed is not None:
-            self.current_embed["footer"] = {"text": text, "icon_url": icon_url}
-
-    def set_color(self, color):
-        if self.current_embed is not None:
-            self.current_embed["color"] = color
-
-    def to_dict(self) -> dict:
+    def to_dict(self):
         data = {
             "username": self.username,
-            "content": self.content
+            "content": self.content,
+            "embeds": [embed.to_dict() for embed in self.embeds],
         }
-        if self.embeds:
-            data["embeds"] =  self.embeds
+
         if self.avatar_url:
             data["avatar_url"] = self.avatar_url
 
-        return data
+        # Remove None values from the dictionary
+        return {k: v for k, v in data.items() if v}
 
     def avatar_to_dict(self):
-        return {
-            "avatar": self.avatar
-        }
+        return {"avatar": self.avatar}
